a70b80adc02974e35ceb2fc247dd5edadbf5b97c
Amaro Marino <rfc1483@gmail.com>
2011-05-19 10:37:52 +0200

    Isolating presentation team table

--- models/find.php
+++ models/find.php
@@ -9,12 +9,12 @@ class Find {
     private $surname;
     private $criteria;
     private $pageMode;
+    private $flecha;
 
     public function writeTable() {
         $this->getAjax();
-        $pageMode = isset($_POST['page_mode']) ? $_POST['page_mode'] : '';
-        $this->setPageMode($pageMode);
-        if ($this->getPageMode() == 'find' || $this->getRefresh() == true) {
+        $this->pageMode = isset($_POST['page_mode']) ? $_POST['page_mode'] : '';
+        if ($this->pageMode == 'find' || $this->refresh == true) {
             $this->showHeaders();
             $this->showRows();
             echo "</table>";
@@ -22,68 +22,45 @@ class Find {
         }
     }
 
-    private __get($key) {
-        return $this->key;
-    }
-    private function getRefresh() {
-        return $this->refresh;
-    }
-
-    private function setPageMode($page_mode) {
-        $this->pageMode = $page_mode;
+    public function __get($property) {
+        if (property_exists($this, $property)) {
+            return $this->$property;
+        }
     }
 
-    private function setRefresh($refresh) {
-        $this->refresh = $refresh;
-    }
+    public function __set($property, $value) {
+        echo "Setting $property to value: $value <br />";
+        if (property_exists($this, $property)) {
+            echo "Setting $property to value: $value";
+            $this->$property = $value;
+        }
 
-    private function getPageMode() {
-        return $this->pageMode;
-    }
-
-    private function getName() {
-        return $this->name;
-    }
-
-    private function getSurname() {
-        return $this->surname;
-    }
-
-    public function setField($field) {
-        $this->field = $field;
-    }
-
-    public function setName($name) {
-        $this->name = $name;
-    }
-
-    public function setSurname($surname) {
-        $this->surname = $surname;
+        return $this;
     }
 
     private function getAjax() {
         // We get the values from the ajax script sent through the GET method
         $this->refresh = false;
         if (isset($_GET['field']) and isset($_GET['sort'])) {
-            $this->setRefresh(true);
+            $this->refresh = true;
             extract($_GET);
-            $this->setField($field);
-            $this->setSort($sort);
-            $this->setName($name);
-            $this->setSurname($surname);
+            $this->field = $field;
+            $this->sort = $sort;
+            $this->name = $name;
+            $this->surname = $surname;
         } else {
             //por defecto
-            $this->setField('name1');
-            $this->setSort('ASC');
-            $this->setName('');
-            $this->setSurname('');
+            $this->field = 'name1';
+            $this->sort = 'ASC';
+            $this->name = '';
+            $this->surname = '';
         }
     }
 
     private function showHeaders() {
-        if ($this->getPageMode() == 'find') {
-            $this->getName() = $_POST['name'];
-            $this->getSurname() = $_POST['surname'];
+        if ($this->pageMode == 'find') {
+            $this->name = $_POST['name'];
+            $this->surname = $_POST['surname'];
         }
 
         echo "<table cellspacing='0' cellpading='0'>";
@@ -99,11 +76,17 @@ class Find {
         $fields = explode(",", $fields);
 
         // Number of elements on the first array.
-        $nroItemsArray = count($fields);
-        $i = 0;
+        $numItemsArray = count($fields);
 
         // Creating the columns
-        while ($i <= $nroItemsArray - 1) {
+        $this->showDataCell($fields, $header, $numItemsArray);
+        echo "</tr>";
+    }
+
+    private function showDataCell($fields, $header, $numItemsArray) {
+        $i = 0;
+        while ($i <= $numItemsArray - 1) {
+
             // Comparing if the field column is the same as the current
             // array item.
             if ($fields[$i] == $this->field) {
@@ -126,12 +109,11 @@ class Find {
             }
             $i++;
         }
-        echo "</tr>";
     }
 
-    // Esta funcion permite comparar el field actual y el nombre de 
-    // la columna en la base de datos
-    private function estilofield($_field, $_columna) {
+    // This function compares the current field and the column's name
+    // in the database.
+    private function fieldStyle($_field, $_columna) {
         if ($_field == $_columna) {
             return " class=\"filas_selec\"";
         } else {
@@ -142,8 +124,7 @@ class Find {
     private function showRows() {
         // Database connection data. 
         require_once 'lib/database.php';
-// Realizamos la consulta de los equipos
-// sortandolos segun field asc o desc
+        // We do the team's query sorting by the 'asc' or 'desc' field
         try {
             $this->criteria = "where 1";
             if (!empty($this->name)) {
@@ -154,26 +135,32 @@ class Find {
                 $this->criteria .= " AND surname1='$this->surname' ";
                 $this->criteria .= " OR surname2='$this->surname' ";
             }
-            $name1 = 'name1';
-            $surname1 = 'surname1';
-            $name2 = 'name2';
-            $surname2 = 'surname2';
-            $dbh = new Database();
-            $sql = "SELECT * FROM team $this->criteria ORDER BY $this->field $this->sort";
-            $sth = $dbh->prepare($sql);
-            $sth->execute();
-            $sth->setFetchMode(PDO::FETCH_ASSOC);
-            //mostramos los resultados mediante la consulta de arriba
-            while ($row = $sth->fetch()) {
-                $id = $row['id'];
-                echo "<tr onclick=\"document.location='modify.php?id=$id';\"> \n";
-                echo "<td" . $this->fieldStyle($this->field, 'name1') . ">$row[$name1] $row[$surname1] / $row[$name2] $row[$surname2]</td> \n";
-                echo "</tr> \n";
-            }
+
+            $this->showDataRow();
         } catch (PDOException $e) {
-            echo "I'm sorry, Dave. I'm afraid I can't do that.";
-            file_put_contents('PDOErrors.txt', $e->getMessage() . "\n", FILE_APPEND);
-            echo $e->getMessage();
+            echo "I'm sorry, Dave. I'm afraid I can't do that. <br />";
+            file_put_contents('PDOErrors.txt', $e->message . "\n", FILE_APPEND);
+            echo $e->message;
+        }
+    }
+
+    private function showDataRow() {
+        $name1 = 'name1';
+        $surname1 = 'surname1';
+        $name2 = 'name2';
+        $surname2 = 'surname2';
+        $dbh = new Database();
+        $sql = "SELECT * FROM team $this->criteria ORDER BY $this->field $this->sort";
+        $sth = $dbh->prepare($sql);
+        $sth->execute();
+        $sth->fetchMode = PDO::FETCH_ASSOC;
+
+        //mostramos los resultados mediante la consulta de arriba
+        while ($row = $sth->fetch()) {
+            $id = $row['id'];
+            echo "<tr onclick=\"document.location='modify.php?id=$id';\"> \n";
+            echo "<td" . $this->fieldStyle($this->field, 'name1') . ">$row[$name1] $row[$surname1] / $row[$name2] $row[$surname2]</td> \n";
+            echo "</tr> \n";
         }
     }
 
